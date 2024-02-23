a7bfaa3c2bee4e6a128275ce10eae7a7a4356171
Victor N <vnoel890@gmail.com>
2024-02-22 20:22:18 -0500

    Add random rain

--- cartoon.js
+++ cartoon.js
@@ -1,15 +1,6 @@
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
 
-// canvas width: 1200 height: 700
-// main hill
-// c.beginPath();
-// c.moveTo(75, 50);
-// c.lineTo(100, 75);
-// c.stroke();
-// c.lineTo(100, 25);
-// c.stroke();
-// c.fill();
 
 
 // back hill
@@ -22,19 +13,6 @@ c.lineTo(0,700)
 c.fillStyle = 'darkgreen'; // Greenish color for the hill
 c.fill();
 
-// // hill
-// var hillHeight = 750;
-// var hillWidth = canvas.clientWidth;
-
-
-// c.beginPath();
-// c.moveTo(305, canvas.clientHeight);
-// // 450 450 1200 700
-// // c.quadraticCurveTo(canvas.clientWidth / 2, canvas.clientHeight - hillHeight, canvas.clientWidth, canvas.clientHeight);
-// c.quadraticCurveTo(650, -50, 1000, canvas.clientHeight);
-// c.fillStyle = '#8CC84B'; // Greenish color for the hill
-// c.fill();
-
 // back hill #2
 c.beginPath();
 c.moveTo(600, 700);
@@ -42,34 +20,25 @@ c.moveTo(600, 700);
 // c.quadraticCurveTo(canvas.clientWidth / 2, canvas.clientHeight - hillHeight, canvas.clientWidth, canvas.clientHeight);
 c.quadraticCurveTo(900, -50, canvas.clientWidth, 400);
 c.lineTo(1200, 700);
-
 c.fillStyle = 'darkgreen'; // Greenish color for the hill
 c.fill();
+
 
 // main hill
 var hillHeight = 750;
 var hillWidth = canvas.clientWidth;
-
 
 c.beginPath();
 c.moveTo(305, canvas.clientHeight);
 // 450 450 1200 700
 // c.quadraticCurveTo(canvas.clientWidth / 2, canvas.clientHeight - hillHeight, canvas.clientWidth, canvas.clientHeight);
 c.quadraticCurveTo(650, -50, 1000, canvas.clientHeight);
-c.fillStyle = '#8CC84B'; // Greenish color for the hill
+c.fillStyle = '#8CC84B';
 c.fill();
 
 
 
-// //sun
-// c.beginPath();
-// c.moveTo(75, 50)
-// c.arc(60, 65, 100, 0, Math.PI * 2, true)
-// c.fillStyle = '#FFD700'; // Yellow color for the sun
-// c.fill();
-
-
-// cloud
+// clouds
 const drawGrayCloud = (x, y) => {
     c.beginPath();
     c.arc(x, y, 60, Math.PI * 0.5, Math.PI * 1.5);
@@ -109,75 +78,25 @@ for (let i=0; i<4; i++) {
 
 // lightning
 
-
-const drawLightning = (context, x, y) => {
-    context.beginPath();
-    context.lineWidth = 5;
-    context.moveTo(x, y);
-    context.lineTo(x - 50, y + 50);
-    context.lineTo(x, y + 50);
-    context.lineTo(x - 50, y + 100);
-    context.lineTo(x + 10, y + 100);
-    context.lineTo(x - 50, y + 250);
-    context.moveTo(x - 5, y + 150);
-    context.lineTo(x + 50, y + 150);
-    context.lineTo(x, y + 350);
-    context.strokeStyle = "yellow";
-    context.stroke();
+const drawLightning = (x, y) => {
+    c.beginPath();
+    c.lineWidth = 5;
+    c.moveTo(x, y);
+    c.lineTo(x - 50, y + 50);
+    c.lineTo(x, y + 50);
+    c.lineTo(x - 50, y + 100);
+    c.lineTo(x + 10, y + 100);
+    c.lineTo(x - 50, y + 250);
+    c.moveTo(x - 5, y + 150);
+    c.lineTo(x + 50, y + 150);
+    c.lineTo(x, y + 350);
+    c.strokeStyle = "yellow";
+    c.stroke();
 }
 
-drawLightning(c, 100, 150);
-drawLightning(c, 800, 150);
-drawLightning(c, 1000, 150);
-
-
-
-//rain
-
-// c.beginPath();
-// c.moveTo(1100, 100);
-// c.lineTo(1080, 150);
-// c.lineWidth = 1;
-// c.strokeStyle = "blue";
-// c.stroke();
-// c.fill();
-
-const rain = (context, x, y) => {
-    context.beginPath();
-    context.moveTo(x, y);
-    context.lineTo(x-20, y+50);
-    context.lineWidth = 1;
-    context.strokeStyle = "blue";
-    context.stroke();
-    context.fill();
-}
-// fix
-let rainX = 100
-let rainY = 100
-for (let i = 0; i<30; i++) {
-    for (let j = 0; j<30; j++) {
-        rain(c, rainX, rainY);
-        rainX+=50; 
-    }
-    rainY+=50;
-}
-
-
-
-// c.beginPath();
-// c.lineWidth = 5;
-// c.moveTo(100,100);
-// c.lineTo(50, 150);
-// c.lineTo(100,150);
-// c.lineTo(50, 200);
-// c.lineTo(110, 200);
-// c.lineTo(50, 350);
-// c.moveTo(95, 250);
-// c.lineTo(150, 250);
-// c.lineTo(100, 450);
-
-// c.strokeStyle = "yellow";
-// c.stroke();
+drawLightning(100, 150);
+drawLightning(800, 150);
+drawLightning(1000, 150);
 
 
 // house
@@ -211,9 +130,26 @@ c.fill();
 c.beginPath();
 c.moveTo(75, 50)
 c.arc(640, 470, 5, 0, Math.PI * 2, true)
-c.fillStyle = '#FFD700'; // Yellow color for the sun
+c.fillStyle = '#FFD700'; 
 c.fill();
 
+
+//rain
+const rain = (x, y) => {
+    c.beginPath();
+    c.moveTo(x, y);
+    c.lineTo(x-20, y+50);
+    c.lineWidth = 1;
+    c.strokeStyle = "blue";
+    c.stroke();
+    c.fill();
+}
+
+for (let j = 0; j < 200; j++) {
+    let rainX = (canvas.width / 30) * j;
+    let rainY = Math.random() * canvas.height; 
+    rain(rainX, rainY);
+}
 
 
 
